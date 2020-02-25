92f9a4fe42202057aeabcea0ca406ab62df9ea09
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-25 00:59:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,3 +6,16 @@ def unsafe?(speed)
   end 
 	
 
+def unsafe?(speed)
+  if speed < 40 == speed > 60 
+    puts "Unsafe!"
+  else speed == 50
+    puts "Safe"
+end
+
+
+def not_safe?(speed)
+	speed < 40 == speed > 60 ? "Unsafe" : "Safe"
+	
+end
+	
